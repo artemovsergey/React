712b1cf60e1885c509d9fe5992c2061e17b32511
artemov <artik3314@gmail.com>
2024-07-27 16:32:38 +0300

    Отправка контейнера api в docker hub

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,22 +1,21 @@
 ﻿#!/bin/bash
-
-# Собираем образы
+echo "Build images from compose"
 docker-compose build #--no-cache --pull
 
 echo "Login to dockerhub: "
-
 docker login -u artik3314 -p Aa003314+ docker.io
 
 echo "Images"
 docker images
 
 echo "Push service Nginx: "
-
 docker tag react_nginx artik3314/react_nginx:latest
 docker push artik3314/react_nginx:latest
 
-
 echo "Push service Angular: "
-
 docker tag react_angular artik3314/react_angular:latest
 docker push artik3314/react_angular:latest
+
+echo "Push service API: "
+docker tag react_api artik3314/react_api:latest
+docker push artik3314/react_api:latest
