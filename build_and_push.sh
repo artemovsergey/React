e7a13910fc42f83ced8409e8b7a8b27f098c421a
artemov <artik3314@gmail.com>
2024-07-27 14:45:04 +0300

    test

--- build_and_push.sh
+++ build_and_push.sh
@@ -8,7 +8,7 @@ SERVICES=$(grep -oP '^\s*\K\w+' docker-compose.yml)
 
 for SERVICE in ${SERVICES}; do
   echo "Building and pushing ${SERVICE}..."
-  docker login -u artik3314 -p Aa003314+
-  docker tag $SERVICE artik3314/$SERVICE:latest
+  #docker login -u artik3314 -p Aa003314+
+  #docker tag $SERVICE artik3314/$SERVICE:latest
   #docker push $DOCKER_HUB_USERNAME/$SERVICE:latest
 done
