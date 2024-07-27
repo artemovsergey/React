00e1510d6f41177c8af0dcb589e6288478dcb3ea
artemov <artik3314@gmail.com>
2024-07-27 15:09:04 +0300

    test

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,9 +3,10 @@
 # Собираем образы
 docker-compose build #--no-cache --pull
 
-# Получаем список сервисов из docker-compose.yml
+echo "Login to dockerhub: "
 
-echo "Building and pushing"
-#docker login -u artik3314 -p Aa003314+
+docker login -u artik3314 -p Aa003314+
 #docker tag $SERVICE artik3314/$SERVICE:latest
-  #docker push $DOCKER_HUB_USERNAME/$SERVICE:latest
+
+echo "Push service: "  
+docker push artik3314/nginx:latest
