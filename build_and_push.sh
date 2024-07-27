f30205a24466715406c2552f53da8f0acc3898cc
artemov <artik3314@gmail.com>
2024-07-27 15:20:46 +0300

    test

--- build_and_push.sh
+++ build_and_push.sh
@@ -8,5 +8,9 @@ echo "Login to dockerhub: "
 docker login -u artik3314 -p Aa003314+
 #docker tag $SERVICE artik3314/$SERVICE:latest
 
+echo "Images"
+docker images
+
 echo "Push service: "
-docker push example-nginx:latest
+
+#docker push example-nginx:latest
