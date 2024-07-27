5d104cbd53a5f2866fd1c032a76a3fdee72faa16
artemov <artik3314@gmail.com>
2024-07-27 15:18:39 +0300

    test

--- build_and_push.sh
+++ build_and_push.sh
@@ -9,6 +9,4 @@ docker login -u artik3314 -p Aa003314+
 #docker tag $SERVICE artik3314/$SERVICE:latest
 
 echo "Push service: "
-
-docker tag nginx example-nginx:tag
 docker push example-nginx:latest
