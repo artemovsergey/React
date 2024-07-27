dc8c99daeefb68b3ed0f8323da4a087a71151a4d
artemov <artik3314@gmail.com>
2024-07-27 16:27:11 +0300

    Отправка контейнера angular  в docker hub

--- build_and_push.sh
+++ build_and_push.sh
@@ -10,7 +10,13 @@ docker login -u artik3314 -p Aa003314+ docker.io
 echo "Images"
 docker images
 
-echo "Push service: "
+echo "Push service Nginx: "
 
 docker tag react_nginx artik3314/react_nginx:latest
 docker push artik3314/react_nginx:latest
+
+
+echo "Push service Angular: "
+
+docker tag angular artik3314/react_angular:latest
+docker push artik3314/react_angular:latest
