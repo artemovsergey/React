6397f833fd50a7b8894fda2a96fc7c418d7164d1
artemov <artik3314@gmail.com>
2024-07-27 15:00:46 +0300

    test

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,11 +4,8 @@
 docker-compose build #--no-cache --pull
 
 # Получаем список сервисов из docker-compose.yml
-SERVICES=$(grep -oP '^services:\n\s*|\n(?<=^services:\n\s*)\w+(?=\s*,|$)' docker-compose.yml)
 
-for SERVICE in ${SERVICES}; do
-  echo "Building and pushing ${SERVICE}..."
-  #docker login -u artik3314 -p Aa003314+
-  #docker tag $SERVICE artik3314/$SERVICE:latest
+echo "Building and pushing"
+#docker login -u artik3314 -p Aa003314+
+#docker tag $SERVICE artik3314/$SERVICE:latest
   #docker push $DOCKER_HUB_USERNAME/$SERVICE:latest
-done
