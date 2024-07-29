9ee9333698a0184f1cdc4f5299893eef9242818a
artemov <artik3314@gmail.com>
2024-07-30 01:29:26 +0300

    VPS

---
+++ vps.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+echo "Go to project"
+cd /home/artik3314/project/
+
+echo "Stop all containers"
+docker-compose -f docker-compose-production.yml down
+
+echo "Update new version images"
+docker-compose -f docker-compose-production.yml pull
+
+echo "Поднятие контейнеров"
+docker-compose -f docker-compose-production.yml up --build
+docker ps
+
+
