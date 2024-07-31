4fa070bf6428e4d1eebb1630ae224f8a90e915a5
artemov <artik3314@gmail.com>
2024-07-31 14:24:03 +0300

    Обновление VPS

--- Example.Application/ApplicationServices.cs
+++ Example.Application/ApplicationServices.cs
@@ -8,7 +8,7 @@ public static class ApplicationServicesRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddLogging();
-
-        return services.AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        return services;
     }
 }
--- Example.Infrastructure/Data/ExampleContextFactory.cs
+++ Example.Infrastructure/Data/ExampleContextFactory.cs
@@ -3,6 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Example.Infrastructure.Data;
+
+/// <summary>
+/// Клаас для работы автогенерации restAPI черезе IDE по модели и контексту
+/// если конструктор с параметрами
+/// </summary>
 public class ExampleContextFactory : IDesignTimeDbContextFactory<ExampleContext>
 {
     public ExampleContext CreateDbContext(string[] args)
